774dd856d614225907204750cdbe8c1eaa516225
faithwest <faithchepkoech425@gmail.com>
2023-10-17 11:32:25 +0300

    rewriting my assignments

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const codingExercises = {
-    frontend: [
-      { name: "Build a Simple Webpage", completed: true },
-      { name: "Create a To-Do List App", completed: false },
-      { name: "Design a Login Form", completed: true },
-      { name: "Implement Responsive Web Design", completed: false },
-      { name: "Build a Weather App", completed: true },
-    ],
-    algorithms: [
-      { name: "Implement Bubble Sort", completed: false },
-      { name: "Solve a Sudoku Puzzle", completed: false },
-      { name: "Implement Binary Search", completed: true },
-      { name: "Write a Recursive Function", completed: true },
-      { name: "Optimize a Sorting Algorithm", completed: false },
-    ],
-    backend: [
-      { name: "Create a RESTful API", completed: false },
-      { name: "Set Up a Database Schema", completed: true },
-      { name: "Implement User Authentication", completed: true },
-      { name: "Build a CRUD Application", completed: false },
-      { name: "Handle API Security", completed: false },
-    ],
-    dataScience: [
-      { name: "Analyze a Dataset", completed: true },
-      { name: "Implement Machine Learning Model", completed: false },
-      { name: "Visualize Data with Matplotlib", completed: true },
-      { name: "Perform Natural Language Processing", completed: false },
-      { name: "Apply Deep Learning Techniques", completed: false },
-    ],
-  };
-//   the goal is to use pure functions as much as possible because they lead to more maintainable and predictable code  
+const itTopics = [
+    {
+      topicName: "Web Development",
+      primaryLanguage: "JavaScript",
+      relatedLanguages: ["HTML", "CSS"],
+      difficultyLevel: "Intermediate",
+    },
+    {
+      topicName: "Data Science",
+      primaryLanguage: "Python",
+      relatedLanguages: ["R", "SQL"],
+      difficultyLevel: "Advanced",
+    },
+    {
+      topicName: "Network Security",
+      primaryLanguage: "C/C++",
+      relatedLanguages: ["Python", "Perl"],
+      difficultyLevel: "Expert",
+    },
+    {
+      topicName: "Cloud Computing",
+      primaryLanguage: "Java",
+      relatedLanguages: ["Python", "Kotlin"],
+      difficultyLevel: "Intermediate",
+    },
+    {
+      topicName: "Machine Learning",
+      primaryLanguage: "Python",
+      relatedLanguages: ["TensorFlow", "PyTorch"],
+      difficultyLevel: "Advanced",
+    },
+  ];
+  
