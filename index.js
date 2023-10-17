282fd8d3e3feb122931e4fe0be0d97513be66f8e
faithwest <faithchepkoech425@gmail.com>
2023-10-17 11:27:49 +0300

    creating objects

---
+++ index.js
@@ -0,0 +1,31 @@
+const codingExercises = {
+    frontend: [
+      { name: "Build a Simple Webpage", completed: true },
+      { name: "Create a To-Do List App", completed: false },
+      { name: "Design a Login Form", completed: true },
+      { name: "Implement Responsive Web Design", completed: false },
+      { name: "Build a Weather App", completed: true },
+    ],
+    algorithms: [
+      { name: "Implement Bubble Sort", completed: false },
+      { name: "Solve a Sudoku Puzzle", completed: false },
+      { name: "Implement Binary Search", completed: true },
+      { name: "Write a Recursive Function", completed: true },
+      { name: "Optimize a Sorting Algorithm", completed: false },
+    ],
+    backend: [
+      { name: "Create a RESTful API", completed: false },
+      { name: "Set Up a Database Schema", completed: true },
+      { name: "Implement User Authentication", completed: true },
+      { name: "Build a CRUD Application", completed: false },
+      { name: "Handle API Security", completed: false },
+    ],
+    dataScience: [
+      { name: "Analyze a Dataset", completed: true },
+      { name: "Implement Machine Learning Model", completed: false },
+      { name: "Visualize Data with Matplotlib", completed: true },
+      { name: "Perform Natural Language Processing", completed: false },
+      { name: "Apply Deep Learning Techniques", completed: false },
+    ],
+  };
+//   the goal is to use pure functions as much as possible because they lead to more maintainable and predictable code  
